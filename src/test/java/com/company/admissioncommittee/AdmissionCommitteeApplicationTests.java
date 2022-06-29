d3b6369dbf02663eb08e054b121fe75ada5e32be
Dwumrohl <81928766+Dwumrohl@users.noreply.github.com>
2022-06-29 14:46:52 +0400

    Initial commit

---
+++ src/test/java/com/company/admissioncommittee/AdmissionCommitteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.admissioncommittee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionCommitteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
